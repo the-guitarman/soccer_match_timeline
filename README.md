1fb50d805759df84dd5e43376be1c08a567eb915
guitarman <guitarman78@freenet.de>
2016-06-12 14:48:07 +0200

    own goal function added

--- README.md
+++ README.md
@@ -75,7 +75,3 @@ This package is licensed under the **LGPL 3.0**. Do whatever you want with it, b
 For commercial usage please contact me at first.
 
 *Note:* Everything may break at every time.
-
-## TODOs
-
-- a goal could be an own goal
--- js/soccer-match-timeline.events.js
+++ js/soccer-match-timeline.events.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
   $(document)
+    .on('click', '.flat-checkbox label', function() {
+    	$(this).toggleClass('checked');
+    	var checkbox = $(this).parent().find('input');
+    	if (checkbox.prop('checked') === 'checked' || checkbox.attr('checked') === 'checked') {
+    		checkbox.removeProp('checked').removeAttr('checked');
+    	} else {
+    		checkbox.prop('checked', true).attr('checked', 'checked');
+    	}
+    })
 	.on('match-event:add', '#match-timeline', function(event, matchEvent) {
       var self = $(this);
       var matchEvents = self.data('match-events') || [];
@@ -18,4 +27,12 @@ $(document).ready(function() {
       matchEventsRenderer.init();
       self.trigger('match-event:afterRemove', removedElementIndex, removedElement, matchEvents);
     })
+    .on('match-event:afterAdd', '#match-timeline', function(event, matchEvent, matchEvents) {
+      matchEventButtonHandler.switchButtons();
+      matchEventButtonHandler.hideEventForm();
+    })
+    .on('match-event:afterRemove', '#match-timeline', function(event, removedElementIndex, removedElement, matchEvents) {
+      matchEventButtonHandler.switchButtons();
+      matchEventButtonHandler.hideEventForm();
+    });
 });
