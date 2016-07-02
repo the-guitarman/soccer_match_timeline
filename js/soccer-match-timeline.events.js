ecee3a1ccacbc935cba499f6ca1533773d9b4b6e
guitarman <guitarman78@freenet.de>
2016-07-02 21:29:14 +0200

    trigger match event bugfix

--- js/soccer-match-timeline.events.js
+++ js/soccer-match-timeline.events.js
@@ -9,29 +9,29 @@ $(document).ready(function() {
     		checkbox.prop('checked', true).attr('checked', 'checked');
     	}
     })
-	.on('match-event:add', '#match-timeline', function(event, matchEvent) {
+	.on('match-event:add', matchEventsElSelector, function(event, matchEvent) {
       var self = $(this);
       var matchEvents = self.data('match-events') || [];
-      self.trigger('match-event:beforeAdd', matchEvent, matchEvents);
+      self.trigger('match-event:beforeAdd', [matchEvent, matchEvents]);
       matchEvents.push(matchEvent);
       self.data('match-events', matchEvents);
       matchEventsRenderer.init();
-      self.trigger('match-event:afterAdd', matchEvent, matchEvents);
+      self.trigger('match-event:afterAdd', [matchEvent, matchEvents]);
     })
-    .on('match-event:remove', '#match-timeline', function(event, removedElementIndex) {
+    .on('match-event:remove', matchEventsElSelector, function(event, removedElementIndex) {
       var self = $(this);
       var matchEvents = self.data('match-events') || [];
       var removedElement = matchEvents.splice(removedElementIndex, 1)[0];
-      self.trigger('match-event:beforeRemove', removedElementIndex, removedElement, matchEvents);
+      self.trigger('match-event:beforeRemove', [removedElementIndex, removedElement, matchEvents]);
       self.data('match-events', matchEvents);
       matchEventsRenderer.init();
-      self.trigger('match-event:afterRemove', removedElementIndex, removedElement, matchEvents);
+      self.trigger('match-event:afterRemove', [removedElementIndex, removedElement, matchEvents]);
     })
-    .on('match-event:afterAdd', '#match-timeline', function(event, matchEvent, matchEvents) {
+    .on('match-event:afterAdd', matchEventsElSelector, function(event, matchEvent, matchEvents) {
       matchEventButtonHandler.switchButtons();
       matchEventButtonHandler.hideEventForm();
     })
-    .on('match-event:afterRemove', '#match-timeline', function(event, removedElementIndex, removedElement, matchEvents) {
+    .on('match-event:afterRemove', matchEventsElSelector, function(event, removedElementIndex, removedElement, matchEvents) {
       matchEventButtonHandler.switchButtons();
       matchEventButtonHandler.hideEventForm();
     });
